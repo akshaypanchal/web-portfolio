84b1e89e00a88ad7375c824d4ac042ebbe2591c8
Akshay <akshaypanchal67@gmail.com>
2019-06-19 22:17:53 -0400

    pic added in about me page

--- web-portfolio/src/components/aboutme.js
+++ web-portfolio/src/components/aboutme.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
+import '../App.css';
+import Tilt from 'react-tilt';
+import Profile  from '../profile.JPG';
 
 class AboutMe extends Component {
+
 	render(){
 		return(
 					<div>
-						<h1>About Me Page</h1>
+						<Grid>
+								<Cell col={6} className="pv2">
+									<Tilt className="Tilt center" options={{ max : 25 }} style={{ height: 500, width: 450 }} >
+ 									<div className="Tilt-inner"><img src ={Profile} style = {{ height: 500, width: 450 }}  /></div>
+							</Tilt>
+								</Cell>
+
+								<Cell col={6}></Cell>
+						</Grid>
 					</div>
 
 			);

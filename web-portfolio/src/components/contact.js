12c692bde20d3691f8f20f0036574d12a7065567
Akshay <akshaypanchal67@gmail.com>
2019-08-10 00:04:24 -0400

    created contact page with all contact details

--- web-portfolio/src/components/contact.js
+++ web-portfolio/src/components/contact.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import contact from '../contact.png';
 
 class Contact extends Component {
 	render(){
 		return(
-					<div>
-						<h1>Contact Pages</h1>
+					<div className="tc">
+						<img alt="contact-icon" src={contact}  widht="200" height="200" />
+						<h2>Akshaykumar Panchal</h2>
+						<h4>Contact No.: +1 (437) 996-4933</h4>
+						<h4>Skype ID: live:akshaypanchal27_1</h4>
+						<h4>Email ID: akshaypanchal67@gmail.com</h4>
+						<h4>Address: 7, Melpham Court, Etobicoke, ON M9V 2H2.</h4>
 					</div>
 
 			);

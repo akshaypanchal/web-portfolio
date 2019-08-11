5631c3913c815b291e65a8fafb62fc5c601ed190
Akshay <akshaypanchal67@gmail.com>
2019-08-11 13:17:14 -0400

    created Resume page and pdf version of my resume with help of react-pdf

--- web-portfolio/src/components/resume.js
+++ web-portfolio/src/components/resume.js
@@ -1,14 +1,52 @@
 import React, {Component} from 'react';
+import {Document, Page} from 'react-pdf';
+import Button from 'react-bootstrap/Button';
+import resume from '../AkshayP_WDev_0808.pdf';
+import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 class Resume extends Component {
-	render(){
-		return(
-					<div>
-						<h1>Resume Page</h1>
-					</div>
+ state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+ 
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
 
-			);
-	}
+   goToPrevPage = () =>
+    					this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    					this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+ 
+  render() {
+    const { pageNumber, numPages } = this.state;
+ 
+    return (
+      <div className="tc">
+        <nav className="tc pa2">
+          <Button variant="outline-primary" className="ma2" onClick={this.goToPrevPage}>Prev</Button>
+          <Button variant="outline-primary" className="ma2" onClick={this.goToNextPage}>Next</Button>
+        </nav>
+
+        <div style={{ width: '50%' , margin:'auto', padding:'10px' }}>
+          <Document
+            file={resume}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
+        </div>
+
+        <p style={{color:'white'}}>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    );
+  }
 }
 
+
 export default Resume;
+
